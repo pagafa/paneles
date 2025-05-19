9d1960231e9654587d1f1792cf06a1955b6bc501
Pablo Gallego Falcón <gallegofalconpablo@gmail.com>
2025-05-19 23:34:29 +0000

    en la pagina principal no se ve el nombre del centro

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,3 +1,4 @@
+
 import { AppLogo } from "@/components/common/AppLogo";
 
 export default function PublicLayout({
@@ -7,7 +8,7 @@ export default function PublicLayout({
 }) {
   return (
     <div className="min-h-screen flex flex-col items-center justify-start bg-gradient-to-br from-background to-secondary/20 p-4 pt-24 md:pt-28">
-       <div className="absolute top-8 left-8">
+       <div className="absolute top-8 left-8 z-50"> {/* Engadido z-50 para asegurar que estea por riba */}
         <AppLogo />
       </div>
       {children}
