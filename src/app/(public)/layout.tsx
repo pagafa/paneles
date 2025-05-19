385c8778e4104a8075b9e14dd7dc9ac572b163ed
Pablo Gallego Falcón <gallegofalconpablo@gmail.com>
2025-05-19 23:35:40 +0000

    sigue sin verse

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -8,7 +8,7 @@ export default function PublicLayout({
 }) {
   return (
     <div className="min-h-screen flex flex-col items-center justify-start bg-gradient-to-br from-background to-secondary/20 p-4 pt-24 md:pt-28">
-       <div className="absolute top-8 left-8 z-50"> {/* Engadido z-50 para asegurar que estea por riba */}
+       <div className="absolute top-8 left-8 z-[60]"> {/* Aumentado z-index a 60 */}
         <AppLogo />
       </div>
       {children}
